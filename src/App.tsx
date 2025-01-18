397f95a7ef1014febc02705315379c7edc40f376
Candice Cirbo <thecirbos@icloud.com>
2025-01-18 13:48:40 -0700

    Test; testing CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Typescript
+          Learn React with Typescript if you dare
         </a>
       </header>
     </div>
