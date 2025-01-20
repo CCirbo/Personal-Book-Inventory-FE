38a967826138fcfd9bcc3706c966b77a778dbf97
Candice Cirbo <thecirbos@icloud.com>
2025-01-20 14:45:34 -0700

    Fix: CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+// import React from 'react';
+import { MemoryRouter } from "react-router-dom";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  // Example check for any key element
+  expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
 });
+
+
+
